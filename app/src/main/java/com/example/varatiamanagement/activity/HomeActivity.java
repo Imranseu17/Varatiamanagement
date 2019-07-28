83e09db1776d5bd8591a0c40684282821d73f23e
imranKhan2018 <imran1995bd@gmail.com>
2019-07-28 20:57:56 +0600

    updated

--- app/src/main/java/com/example/varatiamanagement/activity/HomeActivity.java
+++ app/src/main/java/com/example/varatiamanagement/activity/HomeActivity.java
@@ -1,13 +1,19 @@
 package com.example.varatiamanagement.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.varatiamanagement.R;
 import com.example.varatiamanagement.databinding.ActivityHomeBinding;
+import com.example.varatiamanagement.utils.ChooseAlertDialog;
+import com.example.varatiamanagement.utils.PromptDialog;
+import com.example.varatiamanagement.utils.SharedDataSaveLoad;
+
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -35,6 +41,46 @@ public class HomeActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        showExitDialog();
+    }
+
+    public void showExitDialog() {
+        new ChooseAlertDialog(this)
+                .setDialogType(PromptDialog.DIALOG_TYPE_WARNING)
+                .setAnimationEnable(true)
+                .setTitleText(R.string.warning)
+                .setContentText(R.string.warning_exit_app)
+                .setNegativeListener(getString(R.string.no), new ChooseAlertDialog.OnNegativeListener() {
+                    @Override
+                    public void onClick(ChooseAlertDialog dialog) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveListener(getString(R.string.yes), new ChooseAlertDialog.OnPositiveListener() {
+                    @Override
+                    public void onClick(ChooseAlertDialog dialog) {
+                       startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                        dialog.dismiss();
+                        finish();
+
+                    }
+                }).show();
     }
 
 
